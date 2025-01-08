22a9554fcec57ae66e14d1cde64a6a0ce9c7565a
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-08 19:53:07 +0100

    development

--- main.go
+++ main.go
@@ -25,11 +25,12 @@ type SearchResult struct {
 var userAgents = []string{}
 
 func randomUserAgent() string {
+	rand.Seed(time.Now().Unix())
 	randNum := rand.Intn(len(userAgents))
 	return userAgents[randNum]
 }
 
-func buildGoogleUrls(searchTerm, countryCode, pages, count int)([]string, error) {
+func buildGoogleUrls(searchTerm, countryCode, languageCode string,  pages, count int)([]string, error) {
 	toScrape := []string{}
 	searchTerm := strings.Trim(searchTerm, " ")
 	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
@@ -45,17 +46,37 @@ func buildGoogleUrls(searchTerm, countryCode, pages, count int)([]string, error)
 	return toScrape, nil
 }
 
-func GoogleScrape(searchTerm, countryCode, languageCode string, pages, count) ([]SearchResult, error) {
-	result := []SearchResult{}
+func GoogleScrape(searchTerm, countryCode, languageCode string, proxyString interface{}, pages, count, backoff int) ([]SearchResult, error) {
+	results := []SearchResult{}
 	resultCounter := 0
 	googlePages, err := buildGoogleUrls(searchTerm, countryCode, languageCode, pages, count)
 	if err != nil {
 		return nil, err
 	}
+	for _, page := range googlePages {
+		res, err := scrapeClientRequest(page, proxyString)
+		if err != nil {
+			return nil, err
+		}
+		data, err := googleResultParsing(res, resultCounter)
+		if err != nil {
+			return nil, err
+		}
+		resultCounter += len(data)
+		for _, result := range data {
+			result = append(results, result)
+		}
+		time.Sleep(time.Duration(backoff) * time.Second)
+	}
+	return results, nil
+}
+
+func scrapeClientRequest(searchUrl string, proxyString interface{}) (*http.Response, error) {
+
 }
 
 func main() {
-	res, err := GoogleScrape("akhil Sharma", "com", "en", 1, 30)
+	res, err := GoogleScrape("akhil Sharma", "com", "en", nill, 1, 30, 10)
 	if err == nil {
 		for _, res := range res {
 			fmt.Println(res)
