71bb447ce3be89e969ecdc44ca6ce1dbf65265af
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-08 10:31:20 +0100

    development

--- main.go
+++ main.go
@@ -29,25 +29,33 @@ func randomUserAgent() string {
 	return userAgents[randNum]
 }
 
-func buildGoogleUrls(searchTerm, countryCode, pages, count int) {
+func buildGoogleUrls(searchTerm, countryCode, pages, count int)([]string, error) {
 	toScrape := []string{}
 	searchTerm := strings.Trim(searchTerm, " ")
 	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
 	if googleBase, found := googleDomains[countryCode]; found {
 		for i := 0, i < pages; i++ {
-			start:= i* count
+			start:= i * count
+			scrapeUrl := fmt.Sprintf("%s%s&num=%d&hl=%s&start=%d&filter=0",googleBase, searchTerm, count, languageCode, start )
 		}
+	}else{
+		err:= fmt.Errorf("country (%s) code not found in google", countryCode)
+		return nil, err
+	}
+	return toScrape, nil
+}
+
+func GoogleScrape(searchTerm, countryCode, languageCode string, pages, count) ([]SearchResult, error) {
+	result := []SearchResult{}
+	resultCounter := 0
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode, languageCode, pages, count)
+	if err != nil {
+		return nil, err
 	}
 }
 
-func GoogleScrape(searchTerm, countryCode, pages, count) ([]SearchResult, error) {
-	result := []SearchResult{}
-	resultCounter := 0
-	googlePages, err := buildGoogleUrls(searchTerm, countryCode, pages, count)
-}
-
 func main() {
-	res, err := GoogleScrape("akhil Sharma", "com", 1, 30)
+	res, err := GoogleScrape("akhil Sharma", "com", "en", 1, 30)
 	if err == nil {
 		for _, res := range res {
 			fmt.Println(res)
