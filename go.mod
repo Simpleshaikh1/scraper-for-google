7aaf6555d4d1e635349a1a0adfce7283d309d2ef
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-08 09:11:55 +0100

    SearchResultStruct

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Simpleshaikh1/scraper-for-google
 
 go 1.23.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.1 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/Puerkitobio/goquery"
+	"math/rand"
+	"net/http"
+	"net/url"
 	"strings"
+	"time"
 )
 
-func main() {
+var googleDomains = map[string]string{}
 
+type SearchResult struct {
+	ResultRand  int
+	ResultUrl   string
+	ResultTitle string
+	ResultDesc  string
+}
+
+var userAgents = []string{}
+
+func randomUserAgent() string {
+	randNum := rand.Intn(len(userAgents))
+	return userAgents[randNum]
+}
+
+func main() {
+	res, err := GoogleScrape("akhil Sharma")
+	if err == nil {
+		for _, res := range res {
+			fmt.Println(res)
+		}
+	}
 }
